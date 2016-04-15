47361d0a5af7c95fe13a493a01c7c76d4456fc9c
nsaifullah <nikhil.saifullah@gmail.com>
2016-04-15 11:32:44 -0700

    Updated prepping for Pinning and Striking validation set.

--- analyze_PS.R
+++ analyze_PS.R
@@ -17,6 +17,30 @@ ps_data[,grep("averagemineheight", names(ps_data))] = as.numeric(ps_data[,grep("
 
 #Merge redundant 'not-found' values within variables. Note: Values like "Unknown" or "Other" are not funneled into "No Value Found"
 ps_data[, "uglocation"] = ifelse(ps_data[, "uglocation"] == "NOT MARKED", "NO VALUE FOUND", ps_data[, "uglocation"])
+ps_data[, "accidenttype"] = ifelse(ps_data[, "accidenttype"] == "NOT ELSEWHERE CLASSIFIED", "NO VALUE FOUND", ps_data[, "accidenttype"])
+ps_data[, "immediatenotificationclass"] = ifelse(ps_data[, "immediatenotificationclass"] == "NOT MARKED", "NO VALUE FOUND", ps_data[, "immediatenotificationclass"])
+ps_data[, "natureofinjury"] = ifelse(ps_data[, "natureofinjury"] == "UNCLASSIFIED,NOT DETERMED", "NO VALUE FOUND", ps_data[, "natureofinjury"])
+ps_data[, "equipmanufacturer"] = ifelse(ps_data[, "equipmanufacturer"] == "Not Reported", "NO VALUE FOUND", ps_data[, "equipmanufacturer"])
 
-#Convert date variables; how to format correctly?
-ps_data[, grep("date", names(ps_data))] = apply(ps_data[, grep("date", names(ps_data))], MARGIN = 2, FUN = function(x) as.Date(x, "%m/%d/%Y"))
+#Convert date variables
+indices_with_date = grep("date", names(ps_data))
+for (i in indices_with_date) {
+  ps_data[,i] = as.Date(ps_data[,i], "%m/%d/%Y")
+}
+
+# MERGE IN OTHER VARIABLES FROM PROTO-ALGORITHM (LIKE KEYWORD FLAGS) CHANGE TO KEEP ANY OTHER ADDTL VARS
+ps_tngSet_intnl = read.csv("X:/Projects/Mining/NIOSH/analysis/data/training/training_set_1_internal.csv")
+ps_data = merge(ps_data, ps_tngSet_intnl, c("mineid", "documentno"))
+remove(ps_tngSet_intnl)
+ps_data = ps_data[, c(-grep("\\.y", names(ps_data)))]
+names(ps_data) = gsub("\\.[x|y]", "", names(ps_data))
+
+ps_data[, "pinion"] = ifelse(grepl("(^| )pinion", ps_data[,"narrative"]), 1, 0)
+ps_data[, "pinner"] = ifelse(grepl("(^| )pinner", ps_data[,"narrative"]), 1, 0)
+ps_data[, "pin"] = ifelse(grepl("(^| )pin(n*)(e|i)[a-z]+", ps_data[,"narrative"]) & (ps_data[, "pinion"] == 1 | ps_data[, "pinner"] == 1), 1, 0)
+ps_data[, "strike"] = ifelse(grepl("str(i|u)(.*)k[a-z]*", ps_data[,"narrative"]), 1, 0)
+ps_data[, "trap"] = ifelse(grepl("( )trap[a-z]*", ps_data[,"narrative"]), 1, 0)
+ps_data[, "keyword"] = ifelse((ps_data[, "trap"] == 1 | ps_data[, "pin"] == 1 | ps_data[, "trap"] == 1), 1, 0)
+ps_data = ps_data[, c(-grep("pinner", names(ps_data)), -grep("pinion", names(ps_data)))]
+
+string_vars = 
