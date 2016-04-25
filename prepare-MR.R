45e18a88fda0799856c327864bfba9f000051a9c
sml2198 <sarah.michael.levine@gmail.com>
2016-04-25 13:09:53 -0700

    appends additional fatalities, removes merge of "internal" training set

--- prepare-MR.R
+++ prepare-MR.R
@@ -5,9 +5,27 @@ library(zoo)
 install.packages("dummies")
 library(dummies)
 
-setwd("X:/Projects/Mining/NIOSH/analysis/data/training/coded_sets/")
-mr.data = read.csv("Training_Set_Maintenance_And_Repair_Accidents_August_2015_2.csv", header = TRUE, sep = ",", nrows = 1001, stringsAsFactors = TRUE)
+# SET PREFERENCES - IMPUTATION METHOD - METHOD 3 IS RANDOM DRAWS FROM DISTRIBUTION (OUR BEST METHOD)
 imputation_method = 3
+
+# LOAD IN CODED TRAINING SET (1000 OBSERVATIONS, CODED FOR "MR")
+setwd("X:/Projects/Mining/NIOSH/analysis/data/training/coded_sets/")
+mr.data = read.csv("Training_Set_Maintenance_And_Repair_Accidents_August_2015_2.csv", header = TRUE, sep = ",", nrows = 1001, stringsAsFactors = FALSE)
+
+# LOAD IN DATASET OF ADDITIONAL FATALITIES (FROM OPEN DATA) TO APPEND TO OUR TRAINING SET - ALL "MR"
+#setwd("X:/Projects/Mining/NIOSH/analysis/data/4_coded/")
+mr.fatalities = read.csv("X:/Projects/Mining/NIOSH/analysis/data/4_coded/coded_MR_fatalities.csv", header = TRUE, sep = ",", nrows = 24, stringsAsFactors = FALSE)
+
+# MAKE SURE TRAINING SET AND FATALITIES DATASETS HAVE ALL THE SAME VAR NAMES BEFORE APPENDING
+mr.data$MR = as.factor(mr.data$M.R.)
+mr.data = mr.data[, c(-match("M.R.", names(mr.data)))]
+mr.data[, "death"] = ifelse(grepl("fatality", mr.data[,"degreeofinjury"]), 1, 0)
+
+# CLEAN UP FATALITIES VARIABLES - DROP VARS NOT PRESENT IN TRAINING SET BEFORE APPENDING
+mr.fatalities$MR = as.factor(mr.fatalities$MR_fatality)
+mr.fatalities = mr.fatalities[, c(-grep("MR_fatality", names(mr.fatalities)), -grep("v56", names(mr.fatalities)),
+                                  -grep("v57", names(mr.fatalities)), -grep("v58", names(mr.fatalities)), 
+                                  -grep("v59", names(mr.fatalities)))]
 
 # CLEAN NARRATIVE FIELDS: DROP REDUNDANT VARS AND KEEP LOWERCASE VERSION
 drops <- c("narrativemodified", "degreeofinjury", "accidentclassification", "accidenttype", "natureofinjury", "mineractivity")
@@ -24,8 +42,11 @@ names(mr.data)[names(mr.data) == 'natureofinjury.1'] = 'natureofinjury'
 mr.data$narrative = tolower(mr.data$narrative)
 names(mr.data)[names(mr.data) == 'mineractivity.1'] = 'mineractivity'
 mr.data$narrative = tolower(mr.data$narrative)
-mr.data$MR = as.factor(mr.data$MR)
 
+# APPEND DATASET OF ADDITIONAL FATALITY OBSERVATIONS FOR TRAINING SET
+mr.data <- rbind(mr.data, mr.fatalities) 
+
+# DESTRING VARIABLES
 mr.data[,grep("numberofemployees", names(mr.data))] = gsub(pattern = ",",replacement =  "", mr.data[,grep("numberofemployees", names(mr.data))])
 mr.data[,grep("numberofemployees", names(mr.data))] = as.numeric(mr.data[,grep("numberofemployees", names(mr.data))])
 mr.data[,grep("methaneliberation", names(mr.data))] = gsub(pattern = ",",replacement =  "", mr.data[,grep("methaneliberation", names(mr.data))])
@@ -33,7 +54,7 @@ mr.data[,grep("methaneliberation", names(mr.data))] = as.numeric(mr.data[,grep("
 mr.data[,grep("averagemineheight", names(mr.data))] = gsub(pattern = ",",replacement =  "", mr.data[,grep("averagemineheight", names(mr.data))])
 mr.data[,grep("averagemineheight", names(mr.data))] = as.numeric(mr.data[,grep("averagemineheight", names(mr.data))])
 
-# Merge redundant 'not-found' values within variables. Note: Values like "Unknown" or "Other" are not funneled into "No Value Found"
+# MERGE REDUNDANT "NO VALUE FOUND" FIELDS IN FACTOR VARIABLES
 mr.data[, "uglocation"] = ifelse(mr.data[, "uglocation"] == "NOT MARKED", "NO VALUE FOUND", mr.data[, "uglocation"])
 mr.data[, "immediatenotificationclass"] = ifelse(mr.data[, "immediatenotificationclass"] == "NOT MARKED", "NO VALUE FOUND", mr.data[, "immediatenotificationclass"])
 mr.data[, "natureofinjury"] = ifelse(mr.data[, "natureofinjury"] == "UNCLASSIFIED,NOT DETERMED", "NO VALUE FOUND", mr.data[, "natureofinjury"])
@@ -46,33 +67,19 @@ for (i in indices_with_date) {
 }
 
 # MERGE IN OTHER VARIABLES FROM PROTO-ALGORITHM (LIKE KEYWORD FLAGS) CHANGE TO KEEP ANY OTHER ADDTL VARS
-mr.pre.coding = read.csv("X:/Projects/Mining/NIOSH/analysis/data/training/training_set_2_internal.csv")
-mr.data = merge(mr.data, mr.pre.coding, c("mineid", "documentno"))
-remove(mr.pre.coding)
-mr.data = mr.data[, c(-grep("\\.y", names(mr.data)))]
-names(mr.data) = gsub("\\.[x]", "", names(mr.data))
+#mr.pre.coding = read.csv("X:/Projects/Mining/NIOSH/analysis/data/training/training_set_2_internal.csv")
+#mr.data = merge(mr.data, mr.pre.coding, c("mineid", "documentno"))
+#remove(mr.pre.coding)
+#mr.data = mr.data[, c(-grep("\\.y", names(mr.data)))]
+#names(mr.data) = gsub("\\.[x]", "", names(mr.data))
+# THERE'S NO NEED TO DO THIS ANYMORE, WE'RE REMAKING ALL THE VARS ANYWAY. - SARAH ON 4/25/2016
 
-# DROP 36 OF 144 VARS - THESE ARE VARIABLES WE CREATED 
-mr.data = mr.data[, c(-grep("repair", names(mr.data)), -grep("rplace", names(mr.data)),
-                      -grep("service", names(mr.data)), -grep("fix", names(mr.data)), 
-                      -grep("upgrade", names(mr.data)), -grep("updte", names(mr.data)), 
-                      -grep("servicing", names(mr.data)), -grep("keyword", names(mr.data)), 
-                      -grep("maintain", names(mr.data)), -grep("roof_bolt", names(mr.data)), 
-                      -grep("rib_hole", names(mr.data)), -grep("instal", names(mr.data)), 
-                      -grep("pain", names(mr.data)), -grep("hoist", names(mr.data)),
-                      -grep("insample", names(mr.data)), -grep("group", names(mr.data)),
-                      -grep("changed", names(mr.data)), -grep("dup", names(mr.data)),
-                      -grep("narrativemodified", names(mr.data)), -grep("narrative_old", names(mr.data)),
-                      -grep("surgery", names(mr.data)), -grep("random", names(mr.data)), 
-                      -grep("v5", names(mr.data)))]
-
-# ADD NEW KEYWORD VARS: 11 NEW VARS (ADDED TO 108)
+# ADD NEW KEYWORD VARS: 11 NEW VARS (ADDED TO 106)
 mr.data[, "repair"] = ifelse(grepl("(^| )r(e|a)pa(i*)r[a-z]*", mr.data[,"narrative"]), 1, 0)
 mr.data[, "rplace"] = ifelse(grepl("(^| )replac(e|i)[a-z]*", mr.data[,"narrative"]), 1, 0)
 mr.data[, "service"] = ifelse(grepl("serviced", mr.data[,"narrative"]) | grepl("servicing", mr.data[,"narrative"]), 1, 0)
 mr.data[, "fix"] = ifelse(grepl("(^| )fix[a-z]*", mr.data[,"narrative"]) & !grepl("(^| )fixture", mr.data[,"narrative"]), 1, 0) 
 mr.data[, "maintain"] = ifelse(grepl("(^| )maintain[a-z]*", mr.data[,"narrative"]), 1, 0)
-
 mr.data[, "roof.bolt"] = ifelse(grepl("(roof|rib)*( )*(bolt)(er|ing| |$|.|,).?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?(^| |e|n)i(s|n|t)(s|n|t)(s|n|t)al[a-z]*", 
   mr.data[,"narrative"]) | grepl("(roof|rib)*( )*(bolt)(er|ing| |$|.|,).?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?(^| |e|n)i(s|n|t)(s|n|t)(s|n|t)al[a-z]*", 
   mr.data[,"narrative"]), 1, 0)  
@@ -80,7 +87,6 @@ mr.data[, "rib.hole"] = ifelse(grepl("(rib)( )*(hole).?.?.?.?.?.?.?.?.?.?.?.?.?.
   mr.data[,"narrative"]) | 
     grepl("(^| |e|n)i(s|n|t)(s|n|t)(s|n|t)al[a-z]*.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?.?(rib)( )*(hole)", mr.data[,"narrative"]), 1, 0)  
 mr.data[, "install"] = ifelse(grepl("(^| |e|n)i(s|n|t)(s|n|t)(s|n|t)al[a-z]*", mr.data[,"narrative"]) & (mr.data[, "rib.hole"] != 1 & mr.data[, "roof.bolt"] != 1), 1, 0) 
-
 mr.data[, "pain"] = ifelse(grepl("(^| )pain( |$|.|,|:|)", mr.data[,"narrative"]), 1, 0)
 mr.data[, "hoist"] = ifelse(((grepl("(^| )hoist[a-z]*", mr.data[,"narrative"]) | grepl("(^| )el(e|a|i)vat(o|e)r*", mr.data[,"narrative"])) & mr.data[, "pain"] == 0), 1, 0) 
 mr.data[, "surgery"] = ifelse((grepl("surger[a-z]*", mr.data[,"narrative"]) | grepl("surgic[a-z]*", mr.data[,"narrative"])) & mr.data[, "pain"] == 0, 1, 0)
@@ -95,28 +101,27 @@ mr.data[, "quarter"] = as.yearqtr(mr.data$accidentdate,"%Y-%m-%d")
 mr.data = mr.data[, c(-grep("calendar", names(mr.data)), -grep("accidentdate", names(mr.data)))]
 # do we want to treat accidenttime a certain way?
 
+# REMOVE IRRELEVANT VARS: 25 OF 116 VARS 
+mr.data = mr.data[, c(-match("directionstominemodified", names(mr.data)), -match("minegascategorycode", names(mr.data)),
+                      -match("nooftailingponds", names(mr.data)), -match("noofproducingpits", names(mr.data)),
+                      -match("longitude", names(mr.data)), -match("latitude", names(mr.data)),
+                      -match("nearesttown", names(mr.data)), -match("minestatus", names(mr.data)),
+                      -grep("secondary", names(mr.data)), -match("minetype", names(mr.data)), 
+                      -match("milesfromoffice", names(mr.data)), -match("primarysiccodesuffix", names(mr.data)),
+                      -match("portableoperationindicator", names(mr.data)), -match("roomandpillarindicator", names(mr.data)),
+                      -match("highwallminerindicator", names(mr.data)), -match("multiplepitsindicator", names(mr.data)),
+                      -match("minersrepindicator", names(mr.data)), -match("coalcormetalmmine", names(mr.data)),
+                      -match("primarycanvasscodedesc", names(mr.data)), -match("primarysiccodesuffix", names(mr.data)),
+                      -match("transferredorterminated", names(mr.data)))]
 
-# REMOVE IRRELEVANT VARS: 24 OF 119 VARS 
-mr.data = mr.data[, c(-grep("directionstominemodified", names(mr.data)), -grep("minegascategorycode", names(mr.data)),
-                      -grep("nooftailingponds", names(mr.data)), -grep("noofproducingpits", names(mr.data)),
-                      -grep("longitude", names(mr.data)), -grep("latitude", names(mr.data)),
-                      -grep("nearesttown", names(mr.data)), -grep("minestatus", names(mr.data)),
-                      -grep("secondary", names(mr.data)), -grep("minetype", names(mr.data)), 
-                      -grep("milesfromoffice", names(mr.data)), -grep("ai_type_desc_old", names(mr.data)),
-                      -grep("portableoperationindicator", names(mr.data)), -grep("roomandpillarindicator", names(mr.data)),
-                      -grep("highwallminerindicator", names(mr.data)), -grep("multiplepitsindicator", names(mr.data)),
-                      -grep("minersrepindicator", names(mr.data)), -grep("coalcormetalmmine", names(mr.data)),
-                      -grep("primarycanvasscodedesc", names(mr.data)), -grep("primarysiccodesuffix", names(mr.data)),
-                      -grep("transferredorterminated", names(mr.data)), -grep("primarysiccodesuffix", names(mr.data)))]
-
-# REMOVE VARS UNIQUE AT OBS. LEVEL - 8 OF 95
+# REMOVE VARS UNIQUE AT OBS. LEVEL - 4 OF 89
 mr.data = mr.data[, c(-match("documentno", names(mr.data)), -match("narrative", names(mr.data)),
                       -match("accidenttime", names(mr.data)), -grep("date", names(mr.data)))]
 
-# SHOULD NOW HAVE 87 VARS, NOW REMOVE REDUNDANT VARS (CODES/IDS WITH CORRESPONDING CLASSES - 26 VARS)
+# SHOULD NOW HAVE 89 VARS, NOW REMOVE REDUNDANT VARS (CODES/IDS WITH CORRESPONDING CLASSES - 30 VARS)
 mr.data = mr.data[, c(-match("operatorid", names(mr.data)), -match("controllerid", names(mr.data)), 
-                      -match("contractorid", names(mr.data)), -match("primarycanvasscode", names(mr.data)),
-                      -match("subunitcode", names(mr.data)), -match("inj_degr_cd_old", names(mr.data)),
+                      -match("primarycanvasscode", names(mr.data)), -match("portablefipsstatecode", names(mr.data)),
+                      -match("subunitcode", names(mr.data)), -match("death", names(mr.data)),
                       -match("fipsstatecode", names(mr.data)), -match("activitycode", names(mr.data)), 
                       -match("injurysourcecode", names(mr.data)), -match("natureofinjurycode", names(mr.data)),
                       -match("bodypartcode", names(mr.data)), -match("degreeofinjurycode", names(mr.data)),
@@ -126,21 +131,20 @@ mr.data = mr.data[, c(-match("operatorid", names(mr.data)), -match("controllerid
                       -match("immediatenotificationcode", names(mr.data)), -match("occupcode3digit", names(mr.data)),
                       -match("fipscountycode", names(mr.data)), -match("officecode", names(mr.data)), 
                       -match("bomstatecode", names(mr.data)), -match("primarysiccode", names(mr.data)), 
-                      -match("primarysiccodegroup", names(mr.data)), -match("oldoccupationcode", names(mr.data)),
-                      -match("portablefipsstatecode", names(mr.data)))]
+                      -match("primarysiccodegroup", names(mr.data)), -match("oldoccupationcode", names(mr.data)))]
 
-# SHOULD NOW HAVE 61 VARS, NOW REMOVE 5 OLD/UNKNOWN VARS 
-mr.data = mr.data[, c(-grep("ai_dt_old", names(mr.data)), -grep("idesc", names(mr.data)), -match("i", names(mr.data)), 
-                      -grep("ai_time_old", names(mr.data)), -grep("ai_acty_cd_old", names(mr.data)),
-                      -grep("portablefipsstatecode", names(mr.data)))]
+# SHOULD NOW HAVE 59 VARS, NOW REMOVE 2 OLD/UNKNOWN VARS 
+mr.data = mr.data[, c(-grep("idesc", names(mr.data)), -match("i", names(mr.data)))]
 
-#We don't use date vars as of yet so no need to store a list of their names, "logical" class vars are missing all obsvtns
+# STORE THE TYPE OF ALL VARIABLES AND CREATE LISTS OF VARS BY TYPE
 var_classes = sapply(mr.data[,names(mr.data)], class)
+# "character" vars are factor variables
 charac_vars = names(var_classes[c(grep("character", var_classes), grep("factor", var_classes))])
+# "logical" vars are missing all observations - these variables will be dropped (0 actually dropped here)
 num_vars = names(var_classes[c(grep("numeric", var_classes), grep("integer", var_classes))])
-mr.data = mr.data[, -grep("logical", var_classes)]
 
 for (i in 1:length(charac_vars)) {
+  print(i)
   mr.data[, charac_vars[i]] = ifelse((mr.data[,charac_vars[i]] == "NO VALUE FOUND" | mr.data[,charac_vars[i]] == "UNKNOWN" | 
                                         mr.data[,charac_vars[i]] == "?" | mr.data[,charac_vars[i]] == ""), NA_character_, as.character(mr.data[,charac_vars[i]]))
   mr.data[, charac_vars[i]] = factor(mr.data[, charac_vars[i]])
