0e6d065d03ce4d8b247210ca046211db15083f06
sml2198 <sarah.michael.levine@gmail.com>
2016-04-28 12:42:51 -0700

    adds downsampling code, boosting capability, ROC plots for random forests

--- CART-RF-MR.R
+++ CART-RF-MR.R
@@ -1,6 +1,5 @@
 # This code performs CaRT, random forest (rf) and boosting analysis on the "training" and "simple"
 # datatsets produced in "prepare-MR.R". 
-
 install.packages("rpart")
 install.packages("rpart.plot")
 install.packages("tree")
@@ -9,6 +8,7 @@ install.packages("ggplot2")
 install.packages("reshape2")
 install.packages("pROC")
 install.packages("ROSE")
+install.packages("adabag")
 library(tree)
 library(randomForest)
 library(ggplot2)
@@ -17,6 +17,7 @@ library(pROC)
 library(ROSE)
 library(rpart)
 library(rpart.plot)
+library(adabag)
 
 ######################################################################################################
 # home computer directory
@@ -39,7 +40,22 @@ which( colnames(train)=="MR" )
 which( colnames(simple)=="MR" )
 
 ######################################################################################################
-# DEFINE RANDOM FOREST (TRUE PROPORTION OF 0'S AND 1'S)
+# CREATE CART FUNCTION WITH RPART AND EXECUTE ON 1ST 800 OBSERVATIONS
+cart <- rpart(MR ~ ., data = simple[1:800,], method="class")
+cart 
+
+# PREDICT ON REMAINING OBSERVATIONS 
+cart.predictions = predict(cart, simple[801:1023,],type="class")
+table(simple[801:1023,1], predicted = cart.predictions)
+
+# PLOT RESULTS & DETAILED PLOT OF SPLITS
+rpart.plot(cart, type=3, extra = 101, fallen.leaves=T)
+printcp(cart) 
+importance(cart)
+summary(cart)
+
+######################################################################################################
+# DEFINE RANDOM FOREST (ON TRUE PROPORTION OF NO'S AND YES'S)
 rf <- randomForest(MR ~ ., data = simple[1:700,], mtry = 8, importance=TRUE, type="class",
                    ntree = 600)
 rf
@@ -63,7 +79,34 @@ rf.oob.predictions = predict(rf, simple[1:700,],type="class")
 table(simple[1:700,1], predicted = rf.oob.predictions)
 
 ######################################################################################################
-# OVERSAMPLE POSITIVE OUTCOMES (MR=1)FOR RANDOM FOREST: GENERATE BALANCED DATA W ROSE
+# DOWNSAMPLE NEGATIVE OUTCOMES (MR=NO) FOR RANDOM FOREST
+nmin = sum(simple$MR == "YES")
+nmin
+
+ctrl <- trainControl(method = "cv", classProbs = TRUE, summaryFunction = twoClassSummary)
+
+rf.downsampled = train(MR ~ ., data = simple[1:700,], method = "rf", ntree = 800,
+                       tuneLength = 10, metric = "ROC", trControl = ctrl, 
+                       strata = simple$MR, sampsize = rep(nmin, 2))
+
+rf.baseline = train(MR ~ ., data = simple[1:700,], method = "rf", ntree = 800,
+                       tuneLength = 10, metric = "ROC", trControl = ctrl)
+
+down.prob = predict(rf.downsampled, simple[701:1023,], type = "prob")[,1]
+down.ROC = roc(response = simple[701:1023,1], predictor = down.prob, levels = rev(levels(simple[701:1023,1])))
+
+base.prob = predict(rf.baseline, simple[701:1023,], type = "prob")[,1]
+base.ROC = roc(response = simple[701:1023,1], predictor = base.prob, levels = rev(levels(simple[701:1023,1])))
+
+plot(down.ROC, col = rgb(1, 0, 0, .5), lwd = 2)
+plot(base.ROC, col = rgb(0, 0, 1, .5), lwd = 2, add = TRUE)
+legend(.4, .4, c("Down-Sampled", "Normal"), lwd = rep(2, 1), col = c(rgb(1, 0, 0, .5), rgb(0, 0, 1, .5)))
+# sensitivity = true-positive rate
+# specificity = false-positive rate
+# not a huge improvement here as you can see...
+
+######################################################################################################
+# OVERSAMPLE POSITIVE OUTCOMES (MR=YES) FOR RANDOM FOREST: GENERATE BALANCED DATA W ROSE
 simple.rosex <- ROSE(MR ~ ., data=simple)$data
 # CHECK IMBALANCE AND SORT RANDOMLY (FOR SHITZNGIGGLES)
 table(simple.rosex$MR)
@@ -71,29 +114,17 @@ rand3 <- runif(nrow(simple.rosex))
 simple.rose <- simple.rosex[order(rand3),]
 
 # DEFINE RF ON ROSE OVERSAMPLED DATA
-rf.rose <- randomForest(MR ~ ., data = simple.rose[1:200,], mtry = 8, ntree = 600)
+rf.rose <- randomForest(MR ~ ., data = simple.rose[1:600,], mtry = 8, ntree = 600)
 rf.rose
 
 # PREDICT ON REMAINING OBSERVATIONS & PLOT THE PREDICTIONS (TOP ROW) VS ACTUALS IN TABLE 
-rf_rose_predictions = predict(rf.rose, simple.rose[201:1023,],type="class")
-table(simple.rose[201:1023,1], predicted = rf_rose_predictions)
-
-plot(rf.rose)
-plot(margin(rf.rose))
-getTree(rf.rose,1, labelVar=TRUE)
+rf_rose_predictions = predict(rf.rose, simple.rose[601:1023,],type="class")
+table(simple.rose[601:1023,1], predicted = rf_rose_predictions)
 
 ######################################################################################################
-# CREATE CART FUNCTION WITH RPART AND EXECUTE ON 1ST 800 OBSERVATIONS
-cart <- rpart(MR ~ ., data = simple[1:800,], method="class")
-cart 
-
-cart.predictions = predict(cart, simple[801:1023,],type="class")
-table(simple[801:1023,1], predicted = cart.predictions)
-
-# PLOT RESULTS & DETAILED PLOT OF SPLITS
-rpart.plot(cart, type=3, extra = 101, fallen.leaves=T)
-printcp(cart) 
-importance(cart)
-summary(cart)
+# USE ADABOOST TO IMPLEMENT BOOSTING ALGORITHM 
+mr.adaboost = boosting(MR ~ ., data = simple[1:400,], boos = F, mfinal = 100, coeflearn = 'Freund')
+adaboost.pred = predict.boosting(mr.adaboost, newdata = simple[401:1023,])
+adaboost.pred$confusion
 
 ######################################################################################################
