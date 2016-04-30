cdfc257eed8a9ab5c9f19c5c62f36ab11b9dd064
sml2198 <sarah.michael.levine@gmail.com>
2016-04-29 20:59:48 -0700

    updates merge to append and confirms same #s at stata code

--- clean-accidents.R
+++ clean-accidents.R
@@ -1,4 +1,4 @@
-# This code pulls in and cleans accidents data and merges accidents with mines.
+# This code pulls in and cleans accidents data and mines data and merges accidents with mines.
 
 ######################################################################################################
 setwd("X:/Projects/Mining/NIOSH/analysis/")
@@ -62,31 +62,42 @@ names(acc.2000.16)[names(acc.2000.16) == 'UG_LOCATION_CD'] <- 'uglocationcode'
 names(acc.2000.16)[names(acc.2000.16) == 'UG_MINING_METHOD'] <- 'ugminingmethod'
 names(acc.2000.16)[names(acc.2000.16) == 'UG_MINING_METHOD_CD'] <- 'ugminingmethodcode'
 
-acc.2000.16$mineid = sprintf("%07d", acc.2000.16$mineid)
+# FORMAT VARS AND MAKE SURE HAS ALL THE SAME VARS AS OPEN DATA
+acc.2000.16$mineid = sprintf("%07s", acc.2000.16$mineid)
 acc.2000.16$documentno = as.character(acc.2000.16$documentno)
 acc.2000.16$documentno = sprintf("%12s", acc.2000.16$documentno)
 acc.2000.16$datasource = "opendata"
-  
+acc.2000.16$oldoccupationcode = ""
+acc.2000.16 = acc.2000.16[, c(-match("closeddocumentno", names(acc.2000.16)), -match("coalcormetalm", names(acc.2000.16)))]
+acc.2000.16 = acc.2000.16[acc.2000.16$calendaryear>1999, ]
+
 ######################################################################################################
 # IMPORT 1983-2013 ACCIDENTS DATA FROM MSHA FOR DATA PRE-2000
 # I wish we could import the raw .txt file but I've tried all the encodings and none work, so for now I converted the .txt into a .csv in Stata and I read that. 
 #acc.83.13 = read.table("data/0_originals/MSHA/rec_2015_06_02/Accidents_1983_2013/Accidents_1983_2013.txt",  header = TRUE, sep = "|",  fileEncoding = "ASCII")
 acc.83.13 = read.csv("data/0_originals/MSHA/rec_2015_06_02/Accidents_1983_2013/Accidents_1983_2013.csv",  header = TRUE, sep = ",",  stringsAsFactors = FALSE)
 
-acc.83.13$mineid = sprintf("%07d", acc.83.16$mineid)
-acc.83.13$documentno = as.character(acc.83.16$documentno)
-acc.83.13$documentno = sprintf("%12s", acc.83.16$documentno)
+# FORMAT VARS AND MAKE SURE HAS ALL THE SAME VARS AS OPEN DATA
+acc.83.13$mineid = sprintf("%07s", acc.83.13$mineid)
+acc.83.13$documentno = as.character(acc.83.13$documentno)
+acc.83.13$documentno = sprintf("%12s", acc.83.13$documentno)
 acc.83.13$datasource = "msha"
+acc.83.13$controllername = ""
+acc.83.13$operatorname = ""
+acc.83.13$fiscalquarter = ""
+acc.83.13$fiscalyear = ""
+acc.83.13$investigationbegindate = ""
 
-# MERGE ACCIDENTS
-accidents = merge(acc.83.13,acc.2000.16,by=c("documentno","mineid"))
-accidents = accidents[, c(-grep("\\.y", names(accidents)))]
-names(accidents) = gsub("\\.[x|y]", "", names(accidents))
+# DROP OBSERVATIONS AFTER 2000 (WE'LL GET THESE FROM THE OPEN DATA)
+acc.83.13 = acc.83.13[acc.83.13$calendaryear<2000, ]
 
+# MERGE ACCIDENTS ( SHOULD HAVE 675902 OBS) AND FORMAT DOC/MINE IDS
+accidents = rbind(acc.83.13, acc.2000.16)
 accidents$mineid = sprintf("%07s", accidents$mineid)
 accidents$documentno = sprintf("%12s", accidents$documentno)
-# We've already done this merge in Stata so we know that obsverations not merged from master data (msha) are all 1983-1999 observations (good) 
-# except for 2 from 2013 and 1 from 2005, and all obserbaions not merged from using data (opendata) are 2013+ (good) except for 1 from 2005, 2008, 
+
+# We've already done this as a merge in Stata so we know that observations not merged from master data (msha) are all 1983-1999 observations (good) 
+# except for 2 from 2013 and 1 from 2005, and all observations not merged from using data (opendata) are 2013+ (good) except for 1 from 2005, 2008, 
 # 2010, 2011, each, and 9 from 2012.
 # mystery doc no's with _merge == 1: (2005): 220052270088 (2013): 220133400002 220140070002 
 # mystery doc no's with _merge == 2: (2005): 220052270088 (2008):  220153560029 (2010): 220153240004 (2011): 220152580041  		
@@ -101,7 +112,6 @@ accidents$problem = ifelse((accidents$documentno == "220052270088" | accidents$d
                             accidents$documentno == "220153080004" | accidents$documentno == "220160040025" |
                             accidents$documentno == "220160680001"), 1, 0)
 names(accidents)[names(accidents) == 'narrativemodified'] <- 'narrative'
-names(accidents)[names(accidents) == 'coalcormetalm'] <- 'coalcormetalmmine'
 
 # MAKE SURE NARRATIVE & CHARACTER VARS ARE ALL LOWERCASE
 accidents$mineractivity = tolower(accidents$mineractivity)
@@ -116,5 +126,90 @@ accidents$immediatenotificationclass = tolower(accidents$immediatenotificationcl
 accidents$equipmanufacturer = tolower(accidents$equipmanufacturer)
 accidents$accidentclassification = tolower(accidents$accidentclassification)
 accidents$occupation = tolower(accidents$occupation)
+
+######################################################################################################
+# IMPORT MINES DATA - 86135 OBSERVATIONS
+mines = read.table("data/0_originals/MSHA/open_data/Mines.txt",  header = TRUE, sep = "|")
+
+names(mines)[names(mines) == 'ASSESS_CTRL_NO'] <- 'assesscontrolno'
+names(mines)[names(mines) == 'AVG_MINE_HEIGHT'] <- 'averagemineheight'
+names(mines)[names(mines) == 'BOM_STATE_CD'] <- 'bomstatecode'
+names(mines)[names(mines) == 'COAL_METAL_IND'] <- 'coalcormetalmmine'
+names(mines)[names(mines) == 'COMPANY_TYPE'] <- 'companytype'
+names(mines)[names(mines) == 'CONG_DIST_CD'] <- 'congdistcode'
+names(mines)[names(mines) == 'CURRENT_103I'] <- 'idesc'
+names(mines)[names(mines) == 'CURRENT_103I_DT'] <- 'idate'
+names(mines)[names(mines) == 'CURRENT_CONTROLLER_BEGIN_DT'] <- 'controllerbegindate'
+names(mines)[names(mines) == 'CURRENT_CONTROLLER_ID'] <- 'controllerid'
+names(mines)[names(mines) == 'CURRENT_CONTROLLER_NAME'] <- 'controllername'
+names(mines)[names(mines) == 'CURRENT_MINE_NAME'] <- 'minename'
+names(mines)[names(mines) == 'CURRENT_MINE_STATUS'] <- 'minestatus'
+names(mines)[names(mines) == 'CURRENT_MINE_TYPE'] <- 'minetype'
+names(mines)[names(mines) == 'CURRENT_OPERATOR_ID'] <- 'operatorid'
+names(mines)[names(mines) == 'CURRENT_OPERATOR_NAME'] <- 'operatorname'
+names(mines)[names(mines) == 'CURRENT_STATUS_DT'] <- 'minestatusdate'
+names(mines)[names(mines) == 'DAYS_PER_WEEK'] <- 'daysperweek'
+names(mines)[names(mines) == 'DIRECTIONS_TO_MINE'] <- 'directionstominemodified'
+names(mines)[names(mines) == 'DISTRICT'] <- 'district'
+names(mines)[names(mines) == 'FIPS_CNTY_CD'] <- 'fipscountycode'
+names(mines)[names(mines) == 'FIPS_CNTY_NM'] <- 'fipscountyname'
+names(mines)[names(mines) == 'HIGHWALL_MINER_USED'] <- 'highwallminerindicator'
+names(mines)[names(mines) == 'HOURS_PER_SHIFT'] <- 'hourspershift'
+names(mines)[names(mines) == 'LATITUDE'] <- 'latitude'
+names(mines)[names(mines) == 'LONGITUDE'] <- 'longitude'
+names(mines)[names(mines) == 'MAINT_SHIFTS_PER_DAY'] <- 'maintenanceshiftsperday'
+names(mines)[names(mines) == 'METHANE_LIBERATION'] <- 'methaneliberation'
+names(mines)[names(mines) == 'MILES_FROM_OFFICE'] <- 'milesfromoffice'
+names(mines)[names(mines) == 'MINE_GAS_CATEGORY_CD'] <- 'minegascategorycode'
+names(mines)[names(mines) == 'MINE_ID'] <- 'mineid'
+names(mines)[names(mines) == 'MINERS_REP_IND'] <- 'minersrepindicator'
+names(mines)[names(mines) == 'MULTIPLE_PITS'] <- 'multiplepitsindicator'
+names(mines)[names(mines) == 'NEAREST_TOWN'] <- 'nearesttown'
+names(mines)[names(mines) == 'NO_EMPLOYEES'] <- 'numberofemployees'
+names(mines)[names(mines) == 'NO_NONPRODUCING_PITS'] <- 'noofnonproducingpits'
+names(mines)[names(mines) == 'NO_PRODUCING_PITS'] <- 'noofproducingpits'
+names(mines)[names(mines) == 'NO_TAILING_PONDS'] <- 'nooftailingponds'
+names(mines)[names(mines) == 'OFFICE_CD'] <- 'officecode'
+names(mines)[names(mines) == 'OFFICE_NAME'] <- 'officename'
+names(mines)[names(mines) == 'PART48_TRAINING'] <- 'part48training'
+names(mines)[names(mines) == 'PILLAR_RECOVERY_USED'] <- 'roomandpillarindicator'
+names(mines)[names(mines) == 'PORTABLE_FIPS_ST_CD'] <- 'portablefipsstatecode'
+names(mines)[names(mines) == 'PORTABLE_OPERATION'] <- 'portableoperationindicator'
+names(mines)[names(mines) == 'PRIMARY_CANVASS'] <- 'primarycanvasscodedesc'
+names(mines)[names(mines) == 'PRIMARY_CANVASS_CD'] <- 'primarycanvasscode'
+names(mines)[names(mines) == 'PRIMARY_SIC_CD'] <- 'primarysiccode'
+names(mines)[names(mines) == 'PRIMARY_SIC'] <- 'primarysicdesc'
+names(mines)[names(mines) == 'PRIMARY_SIC_CD_1'] <- 'primarysiccodegroup'
+names(mines)[names(mines) == 'PRIMARY_SIC_CD_SFX'] <- 'primarysiccodesuffix'
+names(mines)[names(mines) == 'PROD_SHIFTS_PER_DAY'] <- 'productionshiftsperday'
+names(mines)[names(mines) == 'SAFETY_COMMITTEE_IND'] <- 'safetycommitteeindicator'
+names(mines)[names(mines) == 'SECONDARY_CANVASS'] <- 'secondarycanvasscodedesc'
+names(mines)[names(mines) == 'SECONDARY_CANVASS_CD'] <- 'secondarycanvasscode'
+names(mines)[names(mines) == 'SECONDARY_SIC'] <- 'secondarysicdesc'
+names(mines)[names(mines) == 'SECONDARY_SIC_CD'] <- 'secondarysiccode'
+names(mines)[names(mines) == 'SECONDARY_SIC_CD_1'] <- 'secondarysiccodegroup'
+names(mines)[names(mines) == 'SECONDARY_SIC_CD_SFX'] <- 'secondarysiccodesuffix'
+names(mines)[names(mines) == 'STATE'] <- 'stateabbreviation'
+
+#GENERATE SOURCE FIELD, FORMAT ID VARS, REMOVE VARS THAT ARE MISSING FOR MOST MINES
+mines = mines[, c(-match("companytype", names(mines)), -match("congdistcode", names(mines)))]
+mines$mineid = sprintf("%07s", mines$mineid)
+
+######################################################################################################
+# MERGE MINES AND ACCIDENTS DATA 
+# SHOULD WIND UP WITH 675887 MINES.ACCIDENTS
+mines.accidents = merge(accidents, mines,by="mineid", all = TRUE)
+
+# KEEP MINES LEVEL DATA FROM MINES DATASET (.y)
+mines.accidents = mines.accidents[, c(-grep("\\.x", names(mines.accidents)))]
+names(mines.accidents) = gsub("\\.[x|y]", "", names(mines.accidents))
+
+# DROP MINES THAT DIDN'T MERGE WITH ANY ACCIDENTS (FROM 739004 TO 675902)
+mines.accidents = mines.accidents[!(is.na(mines.accidents$documentno) | mines.accidents$documentno==""), ]
+mines.accidents = mines.accidents[!(is.na(mines.accidents$mineid) | mines.accidents$mineid==""), ]
+
+# DROP PROBLEM OBSERVATIONS
+mines.accidents = mines.accidents[mines.accidents$problem!=1, ]
+mines.accidents = mines.accidents[, c(-match("problem", names(mines.accidents)))]
 
 ######################################################################################################
