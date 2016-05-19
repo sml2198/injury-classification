f07a11addc85c2ee161f01462af056e9ab625763
nsaifullah <nikhil.saifullah@gmail.com>
2016-05-19 07:51:32 -0700

    continues building merging functionality. More updates to come shortly.

--- merge_violations_mines.R
+++ merge_violations_mines.R
@@ -5,12 +5,19 @@ clean_violations = readRDS("X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/cle
 clean_mines = readRDS("X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_mines.rds")
 clean_Inspections = readRDS("X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_Inspections.rds")
 
-#is beginningdate defined for all rows in each?
 assessments_violations = merge(clean_assessments, clean_violations, by = "violationno", all = T)
-assessments_violations[, "merge"] = ifelse(!is.na(assessments_violations$beginningdate.y) & !is.na(assessments_violations$beginningdate.x), 3, 0)
-assessments_violations[, "merge"] = ifelse(is.na(assessments_violations$beginningdate.x) & !is.na(assessments_violations$beginningdate.y), 2, assessments_violations[, "merge"])
-assessments_violations[, "merge"] = ifelse(is.na(assessments_violations$beginningdate.y) & !is.na(assessments_violations$beginningdate.x), 1, assessments_violations[, "merge"])
-table(assessments_violations$merge)
+assessments_violations[, "merge"] = ifelse(!is.na(assessments_violations$mineid.y) & !is.na(assessments_violations$mineid.x), 3, 0)
+assessments_violations[, "merge"] = ifelse(is.na(assessments_violations$mineid.x) & !is.na(assessments_violations$mineid.y), 2, assessments_violations[, "merge"])
+assessments_violations[, "merge"] = ifelse(is.na(assessments_violations$mineid.y) & !is.na(assessments_violations$mineid.x), 1, assessments_violations[, "merge"])
+table(assessments_violations$merge) 
+#table(assessments_violations[assessments_violations$calendaryear >= 2000,]$merge) aligns with the STATA version except for the 43 pre-2000 open data 
+#observations noted at the end of "clean_violations.do"
+common_varstbs = sub(".x", "", names(assessments_violations)[grep(".x", names(assessments_violations), fixed = T)], fixed = T)
+for (i in 1:length(common_varstbs)) {
+  assessments_violations[, paste(common_varstbs[i], ".x", sep = "")] = ifelse(assessments_violations[, "merge"] == 2, assessments_violations[, paste(common_varstbs[i], ".y", sep = "")], assessments_violations[, paste(common_varstbs[i], ".x", sep = "")])
+}
+assessments_violations = assessments_violations[, -grep(".y", names(assessments_violations), fixed = T)]
+names(assessments_violations)[grep(".x", names(assessments_violations), fixed = T)] = common_varstbs
 
 assessments_violations[, "violatorname"] = tolower(str_trim(assessments_violations[, "violatorname"], side = c("both")))
 assessments_violations[, "violator_name"] = tolower(str_trim(assessments_violations[, "violator_name"], side = c("both")))
