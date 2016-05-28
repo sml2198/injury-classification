29de1528da84ed2b9260150e4f5a569fb42a6a66
nsaifullah <nikhil.saifullah@gmail.com>
2016-05-27 17:10:39 -0700

    blocks out non-working code

--- analyze_PS_R.R
+++ analyze_PS_R.R
@@ -391,8 +391,8 @@ ps_data$num.person <- str_count(ps_data$narrative, "PERSON")
 ps_data$num.body <- str_count(ps_data$narrative, "BODY")
 
 #This needs to be made robust to no-vehicle rows and to work with the columns of a data frame rather than treat ps_data$narrative as one big string
-ps_data$num_unique_vehcl = length(unique(substr(unlist(regmatches(ps_data$narrative, gregexpr("VEHICLE[0-9][0-9]*", ps_data$narrative))), 8, 9)))
-ps_data$mult_vehcl = ifelse(ps_data$num_unique_vehcl > 1, 1, 0)
+#ps_data$num_unique_vehcl = length(unique(substr(unlist(regmatches(ps_data$narrative, gregexpr("VEHICLE[0-9][0-9]*", ps_data$narrative))), 8, 9)))
+#ps_data$mult_vehcl = ifelse(ps_data$num_unique_vehcl > 1, 1, 0)
 
 # CREATE A FEW MORE KEYWORDS ON THE NEW NARRATIVE FIELDS
 ps_data[, "loose_rbolting"] = ifelse(grepl("(plate|bit|bolt)+.{1,10}PINNED/STRUCK", ps_data[,"narrative"]), 1, 0)
