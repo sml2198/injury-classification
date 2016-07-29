3e1e14b24088cb5049fb0de598de9dc1f2143c9c
juliabodson <juliabodson@gmail.com>
2016-07-29 11:26:08 -0700

    Dressed in all pink, 'cept my gator shoes, those are green

--- 3_merge_accidents.R
+++ 3_merge_accidents.R
@@ -11,36 +11,36 @@ setwd("X:/Projects/Mining/NIOSH/analysis/")
 
 # define file names
   # input: clean and merged accidents data
-accidents.file.name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_accidents.rds" 
+accidents_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_accidents.rds" 
   # input: clean and merged mines data
-mines.file.name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_mines.rds" 
+mines_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_mines.rds" 
   # output: merged mines and accidents data
-mines.accidents.file.name = "X:/Projects/Mining/NIOSH/analysis/data/3_merged/merged_mines_accidents.rds"
+mines_accidents_file_name = "X:/Projects/Mining/NIOSH/analysis/data/3_merged/merged_mines_accidents.rds"
 
 ######################################################################################################
 
 # MERGE MINES AND ACCIDENTS DATA, THEN OUTPUT
 
 # read data files
-accidents = readRDS(accidents.file.name)
-mines_quarters = readRDS(mines.file.name)
+accidents = readRDS(accidents_file_name)
+mines_quarters = readRDS(mines_file_name)
 
 # merge mines and accidents data
-mines.accidents = merge(accidents, mines_quarters,by = "mineid", all = TRUE) # should have 675887 observations
+mines_accidents = merge(accidents, mines_quarters,by = "mineid", all = TRUE) # should have 675887 observations
 
 # keep mine-level information from mines data (.y)
-mines.accidents = mines.accidents[, c(-grep("\\.x", names(mines.accidents)))]
-names(mines.accidents) = gsub("\\.[x|y]", "", names(mines.accidents))
+mines_accidents = mines_accidents[, c(-grep("\\.x", names(mines_accidents)))]
+names(mines_accidents) = gsub("\\.[x|y]", "", names(mines_accidents))
 
 # drop mines that don't merge with any accidents (from 739004 to 675902)
-mines.accidents = mines.accidents[!(is.na(mines.accidents$documentno) | mines.accidents$documentno == ""), ]
-mines.accidents = mines.accidents[!(is.na(mines.accidents$mineid) | mines.accidents$mineid == ""), ]
+mines_accidents = mines_accidents[!(is.na(mines_accidents$documentno) | mines_accidents$documentno == ""), ]
+mines_accidents = mines_accidents[!(is.na(mines_accidents$mineid) | mines_accidents$mineid == ""), ]
 
 # drop problematic observations
-mines.accidents = mines.accidents[mines.accidents$problem != 1, ]
-mines.accidents = mines.accidents[, c(-match("problem", names(mines.accidents)))]
+mines_accidents = mines_accidents[mines_accidents$problem != 1, ]
+mines_accidents = mines_accidents[, c(-match("problem", names(mines_accidents)))]
 
 # output merged mines and accidents data
-saveRDS(mines.accidents, file = mines.accidents.file.name)
+saveRDS(mines_accidents, file = mines_accidents_file_name)
 
 ######################################################################################################
