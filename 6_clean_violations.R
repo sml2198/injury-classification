cf21c95203dd50cb0ff83897db3ce8abde87d531
juliabodson <juliabodson@gmail.com>
2016-07-29 12:01:52 -0700

    "We'll never go out of style" - Bob Marley

--- 6_clean_violations.R
+++ 6_clean_violations.R
@@ -21,9 +21,8 @@ open_data_viols_out_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleane
 # read open source violations data
   # dataset downloaded on 4/20/16 from http://arlweb.msha.gov/OpenGovernmentData/OGIMSHA.asp [MSHA open data platform (Violations)]
 open_data_viols = read.table(open_data_viols_in_file_name, header = T, sep = "|")
-#early_viols = read.csv("X:/Projects/Mining/NIOSH/analysis/data/1_converted/MSHA/violations_fromText.csv")
 
-# Rename variables (we did this originally so var names would be consistent with our existing data pull - this is mostly cosmetic)
+# rename variables
 names(open_data_viols)[names(open_data_viols) == "VIOLATION_NO"] = "violationno"
 names(open_data_viols)[names(open_data_viols) == "VIOLATION_ID"] = "violationid"
 names(open_data_viols)[names(open_data_viols) == "EVENT_NO"] = "eventno"
@@ -57,25 +56,23 @@ names(open_data_viols)[names(open_data_viols) == "CONTESTED_IND"] = "contestedin
 names(open_data_viols)[names(open_data_viols) == "MINE_NAME"] = "minename"
 names(open_data_viols)[names(open_data_viols) == "MINE_TYPE"] = "minetype"
 
-# Make all varnames lowercase because Sarah hates capitalization
+# format variables
 names(open_data_viols) = tolower(names(open_data_viols))
 
-# Flag and drop duplicates on violation number
+# flag and drop duplicates on violation number
 open_data_viols[, "dup"] = duplicated(open_data_viols$violationno)
-table(open_data_viols$dup) #Same duplicates as in the STATA version of the code so no testing here. - Nikhil
-open_data_viols = open_data_viols[open_data_viols$dup == F,]
+table(open_data_viols$dup) # same duplicates as in the STATA version of the code
+open_data_viols = open_data_viols[open_data_viols$dup == F, ]
 
-# Format violationo and eventno
+# drop data from  environments not of interest
+  # facility means a mill/processing location, always above ground, according to April Ramirez @ DOL on 6/6/16
+open_data_viols = open_data_viols[open_data_viols$coalcormetalm == "C", ]
+open_data_viols = open_data_viols[!is.na(open_data_viols$coalcormetalm), ]
+open_data_viols = open_data_viols[open_data_viols$minetype == "Underground", ]
+
+# format variables
 open_data_viols[, "violationno"] = as.character(open_data_viols[, "violationno"])
 open_data_viols[, "eventno"] = as.character(open_data_viols[, "eventno"])
-
-# Only keep observations from environment we care about
-open_data_viols = open_data_viols[open_data_viols$coalcormetalm == "C",]
-open_data_viols = open_data_viols[!is.na(open_data_viols$coalcormetalm),]
-# (Facility means a mill/processing location, always above ground, according to April Ramirez @ DOL on 6/6/16)
-open_data_viols = open_data_viols[open_data_viols$minetype == "Underground",]
-
-# Format mineid, violationo and eventno as 7 digit numbers padded with zeroes
 open_data_viols$mineid = str_pad(open_data_viols$mineid, 7, pad = "0")
 open_data_viols$mineid = withr::with_options(c(scipen = 999), str_pad(open_data_viols$mineid, 7, pad = "0"))
 open_data_viols$eventno = str_pad(open_data_viols$eventno, 7, pad = "0")
@@ -83,34 +80,37 @@ open_data_viols$eventno = withr::with_options(c(scipen = 999), str_pad(open_data
 open_data_viols$violationno = str_pad(open_data_viols$violationno, 7, pad = "0")
 open_data_viols$violationno = withr::with_options(c(scipen = 999), str_pad(open_data_viols$violationno, 7, pad = "0"))
 
+# output clean violations data
 saveRDS(open_data_viols, file = open_data_viols_out_file_name)
 
 ######################################################################################################
-# THIS CODE IS RETIRED
-# The portions commented out are to load and clean data from Carolyn Stasik's (MSHA) data pull from May 20th, 2015
 
+# THIS CODE IS RETIRED # 
+# this code loads and cleans data from Carolyn Stasik's (MSHA) data pull from May 20th, 2015
 
-#Keep only observations from Carolyn Stasik's data in years prior to the Open Data's time range
-#early_viols = early_viols[early_viols$calendaryear <= 1999 & early_viols$calendaryear >= 1983,]
+# early_viols = read.csv("X:/Projects/Mining/NIOSH/analysis/data/1_converted/MSHA/violations_fromText.csv")
 
-#clean_violations = merge(early_viols, open_data_viols, by = "violationno", all = T)
-#clean_violations[, "merge"] = ifelse(!is.na(clean_violations$eventno.y) & !is.na(clean_violations$eventno.x), 3, 0)
-#clean_violations[, "merge"] = ifelse(is.na(clean_violations$eventno.x) & !is.na(clean_violations$eventno.y), 2, clean_violations[, "merge"])
-#clean_violations[, "merge"] = ifelse(is.na(clean_violations$eventno.y) & !is.na(clean_violations$eventno.x), 1, clean_violations[, "merge"])
-#table(clean_violations$merge) #1 observation in STATA's open data as compared with open_data_assessments. Nikhil 5/13/16
-#1       2       3 
-#2276578 2161593      43
+# Keep only observations from Carolyn Stasik's data in years prior to the Open Data's time range
+# early_viols = early_viols[early_viols$calendaryear <= 1999 & early_viols$calendaryear >= 1983,]
 
-#For the 43 pre-2000 observations in both datasets, use the open data
-#common_varstbs = sub(".x", "", names(clean_violations)[grep(".x", names(clean_violations), fixed = T)], fixed = T)
-#for (i in 1:length(common_varstbs)) {
-#  mstr_copy = paste(common_varstbs[i], ".x", sep = "")
-#  usng_copy = paste(common_varstbs[i], ".y", sep = "")
-#  clean_violations[, mstr_copy] = ifelse(clean_violations[, "merge"] == 3, clean_violations[, usng_copy], clean_violations[, mstr_copy])
-#  clean_violations[, mstr_copy] = ifelse(clean_violations[, "merge"] == 2, clean_violations[, usng_copy], clean_violations[, mstr_copy])
-#}
-#clean_violations = clean_violations[, -grep(".y", names(clean_violations), fixed = T)]
-#names(clean_violations)[grep(".x", names(clean_violations), fixed = T)] = common_varstbs
-#clean_violations = clean_violations[, -grep("merge", names(clean_violations))]
+# clean_violations = merge(early_viols, open_data_viols, by = "violationno", all = T)
+# clean_violations[, "merge"] = ifelse(!is.na(clean_violations$eventno.y) & !is.na(clean_violations$eventno.x), 3, 0)
+# clean_violations[, "merge"] = ifelse(is.na(clean_violations$eventno.x) & !is.na(clean_violations$eventno.y), 2, clean_violations[, "merge"])
+# clean_violations[, "merge"] = ifelse(is.na(clean_violations$eventno.y) & !is.na(clean_violations$eventno.x), 1, clean_violations[, "merge"])
+# table(clean_violations$merge) #1 observation in STATA's open data as compared with open_data_assessments. Nikhil 5/13/16
+# 1       2       3 
+# 2276578 2161593      43
+
+# For the 43 pre-2000 observations in both datasets, use the open data
+# common_varstbs = sub(".x", "", names(clean_violations)[grep(".x", names(clean_violations), fixed = T)], fixed = T)
+# for (i in 1:length(common_varstbs)) {
+#   mstr_copy = paste(common_varstbs[i], ".x", sep = "")
+#   usng_copy = paste(common_varstbs[i], ".y", sep = "")
+#   clean_violations[, mstr_copy] = ifelse(clean_violations[, "merge"] == 3, clean_violations[, usng_copy], clean_violations[, mstr_copy])
+#   clean_violations[, mstr_copy] = ifelse(clean_violations[, "merge"] == 2, clean_violations[, usng_copy], clean_violations[, mstr_copy])
+# }
+# clean_violations = clean_violations[, -grep(".y", names(clean_violations), fixed = T)]
+# names(clean_violations)[grep(".x", names(clean_violations), fixed = T)] = common_varstbs
+# clean_violations = clean_violations[, -grep("merge", names(clean_violations))]
 
 ######################################################################################################
