c74ab82e17089281e0a7300a182db1714cf9e223
sml2198 <sarah.michael.levine@gmail.com>
2016-08-02 23:14:00 -0700

    merges mines and accidents instead on accidents and mine-quarters, adds comments about unique obs, etc

--- 3_merge_accidents.R
+++ 3_merge_accidents.R
@@ -7,11 +7,13 @@
 
 ######################################################################################################
 
+library(plyr)
+
 # define file names
   # input: clean and merged accidents data (2_clean_accidents)
 accidents_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_accidents_TEST.rds" 
-  # input: clean and merged mines data (1_clean_mines)
-mines_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_mines_TEST.rds" 
+  # input: clean and merged mines data (1_clean_mines) - ALL mines
+mines_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/all_clean_mines_TEST.rds" 
   # output: merged mines and accidents data
 mines_accidents_file_name = "X:/Projects/Mining/NIOSH/analysis/data/3_merged/merged_mines_accidents_TEST.rds"
 
@@ -20,25 +22,60 @@ mines_accidents_file_name = "X:/Projects/Mining/NIOSH/analysis/data/3_merged/mer
 # MERGE MINES AND ACCIDENTS DATA, THEN OUTPUT
 
 # read data files
-accidents = readRDS(accidents_file_name)
-mines_quarters = readRDS(mines_file_name)
+accidents = readRDS(accidents_file_name) #284,109 obs, 58 vars, 5592 unique mines
+mines_quarters = readRDS(mines_file_name) #50,890 obs, 68 vars, 14,508 unique mines
+
+# collapse mines data to the mine level (no quarters required) - these are already mine-level vars
+temp = mines_quarters[, c("mineid", 
+                          "year",
+                          "quarter",
+                          "minetype",
+                          "minename",
+                          "minestatus",
+                          "minestatusdate",
+                          "operatorid",
+                          "operatorname",
+                          "coalcormetalmmine",
+                          "stateabbreviation",
+                          "idate",
+                          "idesc",
+                          "daysperweek",
+                          "productionshiftsperday")]
+
+# average vars that are quarter-specific - wind up with 1587 observations (equal to number of unique mines, as it should be)
+vars_to_avrg = ddply(mines_quarters[, c("hours_qtr",
+                                          "employment_qtr", 
+                                          "coal_prod_qtr", 
+                                          "mineid")], c("mineid"), 
+                       function(x) colMeans(x[, c(match("hours_qtr", names(x)), 
+                                                  match("employment_qtr", names(x)), 
+                                                  match("coal_prod_qtr", names(x)))], na.rm = TRUE))
+
+# rename quarter specific vars so we don't get confused 
+names(vars_to_avrg)[names(vars_to_avrg) == "hours_qtr"] = "avg_hours_qtr"
+names(vars_to_avrg)[names(vars_to_avrg) == "employment_qtr"] = "avg_employment_qtr"
+names(vars_to_avrg)[names(vars_to_avrg) == "coal_prod_qtr"] = "avg_coal_prod_qtr"
+
+mines = merge(vars_to_avrg, temp, by = "mineid") # should have 50,890 observations, 18 vars, 14,508 unique mines
+
+# keep unique mine info for 14,508 unique mines
+mines = mines[!duplicated(mines$mineid), ]
+
+# now remove useless datasets
+rm(mines_quarters, temp, vars_to_avrg)
 
 # merge mines and accidents data
-mines_accidents = merge(accidents, mines_quarters,by = "mineid", all = TRUE) # should have 4659780 observations
+mines_accidents = merge(accidents, mines, by = "mineid") # should have 199,022 obs of unique accidents, 4829 unique mines
 
 # keep mine-level information from mines data (.y)
 mines_accidents = mines_accidents[, c(-grep("\\.x", names(mines_accidents)))]
 names(mines_accidents) = gsub("\\.[x|y]", "", names(mines_accidents))
 
-# drop mines that don't merge with any accidents (from 739004 to 675902)
-mines_accidents = mines_accidents[!(is.na(mines_accidents$documentno) | mines_accidents$documentno == ""), ]
-mines_accidents = mines_accidents[!(is.na(mines_accidents$mineid) | mines_accidents$mineid == ""), ]
-
-# drop problematic observations
+# drop problematic observations (from 199,022 to 199,019)
 mines_accidents = mines_accidents[mines_accidents$problem != 1, ]
 mines_accidents = mines_accidents[, c(-match("problem", names(mines_accidents)))]
 
-# output merged mines and accidents data
+# output merged mines and accidents data - 199,019 unique accidents, 4829 unique mines, 72 vars
 saveRDS(mines_accidents, file = mines_accidents_file_name)
 
 ######################################################################################################
