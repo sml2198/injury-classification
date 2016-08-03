361b37313e88ba28adffa98b0c740344784bf9cb
juliabodson <juliabodson@gmail.com>
2016-08-03 10:46:52 -0700

    removed retired code, commented #obs (etc), removed TEST suffixes... SO PRETTY!

--- 6_clean_violations.R
+++ 6_clean_violations.R
@@ -14,26 +14,24 @@ library(stringr)
 open_data_viols_in_file_name = "X:/Projects/Mining/NIOSH/analysis/data/0_originals/MSHA/open_data/Violations.txt"
   # output: clean violations data
     # uniquely identified by eventno (inspection number) * violationno (violation number)
-open_data_viols_out_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_violations_TEST.rds"
+open_data_viols_out_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_violations.rds"
 
 ######################################################################################################
 
 # READ AND CLEAN VIOLATIONS DATA, THEN OUTPUT
 
-# read violations data
+# read violations data - 2161687 obs, 61 var
   # dataset downloaded on 4/20/16 from http://arlweb.msha.gov/OpenGovernmentData/OGIMSHA.asp [MSHA open data platform (Violations)]
 open_data_viols = read.table(open_data_viols_in_file_name, header = T, sep = "|")
 
 # drop data from environments not of interest
   # facility means a mill/processing location, always above ground, according to April Ramirez @ DOL on 6/6/16
-open_data_viols = open_data_viols[open_data_viols$COAL_METAL_IND == "C", ]
-open_data_viols = open_data_viols[!is.na(open_data_viols$COAL_METAL_IND), ]
-open_data_viols = open_data_viols[open_data_viols$MINE_TYPE == "Underground", ]
+open_data_viols = open_data_viols[open_data_viols$COAL_METAL_IND == "C", ] # drop 980975 obs
+open_data_viols = open_data_viols[open_data_viols$MINE_TYPE == "Underground", ] # drop 314320 obs
 
 # flag and drop duplicates on violationno
-open_data_viols[, "dup"] = duplicated(open_data_viols$VIOLATION_NO)
-table(open_data_viols$dup) # same duplicates as in the STATA version of the code
-open_data_viols = open_data_viols[open_data_viols$dup == F, ]
+open_data_viols[, "dup"] = duplicated(open_data_viols$VIOLATION_NO) # 866392
+open_data_viols = open_data_viols[open_data_viols$dup == F, ] # drop 35 obs
 
 # rename variables
 names(open_data_viols)[names(open_data_viols) == "VIOLATION_NO"] = "violationno"
@@ -77,38 +75,7 @@ open_data_viols$mineid = str_pad(open_data_viols$mineid, 7, pad = "0")
 open_data_viols$eventno = str_pad(open_data_viols$eventno, 7, pad = "0")
 open_data_viols$violationno = str_pad(open_data_viols$violationno, 7, pad = "0")
 
-# output clean violations data
+# output clean violations data - 866357 obs, 62 vars, 1688 unique mines
 saveRDS(open_data_viols, file = open_data_viols_out_file_name)
-
-######################################################################################################
-
-# THIS CODE IS RETIRED # 
-
-# this code loads and cleans data from Carolyn Stasik's (MSHA) data pull from May 20th, 2015
-
-# early_viols = read.csv("X:/Projects/Mining/NIOSH/analysis/data/1_converted/MSHA/violations_fromText.csv")
-
-# Keep only observations from Carolyn Stasik's data in years prior to the Open Data's time range
-# early_viols = early_viols[early_viols$calendaryear <= 1999 & early_viols$calendaryear >= 1983,]
-
-# clean_violations = merge(early_viols, open_data_viols, by = "violationno", all = T)
-# clean_violations[, "merge"] = ifelse(!is.na(clean_violations$eventno.y) & !is.na(clean_violations$eventno.x), 3, 0)
-# clean_violations[, "merge"] = ifelse(is.na(clean_violations$eventno.x) & !is.na(clean_violations$eventno.y), 2, clean_violations[, "merge"])
-# clean_violations[, "merge"] = ifelse(is.na(clean_violations$eventno.y) & !is.na(clean_violations$eventno.x), 1, clean_violations[, "merge"])
-# table(clean_violations$merge) #1 observation in STATA's open data as compared with open_data_assessments. Nikhil 5/13/16
-# 1       2       3 
-# 2276578 2161593      43
-
-# For the 43 pre-2000 observations in both datasets, use the open data
-# common_varstbs = sub(".x", "", names(clean_violations)[grep(".x", names(clean_violations), fixed = T)], fixed = T)
-# for (i in 1:length(common_varstbs)) {
-#   mstr_copy = paste(common_varstbs[i], ".x", sep = "")
-#   usng_copy = paste(common_varstbs[i], ".y", sep = "")
-#   clean_violations[, mstr_copy] = ifelse(clean_violations[, "merge"] == 3, clean_violations[, usng_copy], clean_violations[, mstr_copy])
-#   clean_violations[, mstr_copy] = ifelse(clean_violations[, "merge"] == 2, clean_violations[, usng_copy], clean_violations[, mstr_copy])
-# }
-# clean_violations = clean_violations[, -grep(".y", names(clean_violations), fixed = T)]
-# names(clean_violations)[grep(".x", names(clean_violations), fixed = T)] = common_varstbs
-# clean_violations = clean_violations[, -grep("merge", names(clean_violations))]
 
 ######################################################################################################
