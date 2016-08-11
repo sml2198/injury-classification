9552985710db3a757557e34b5dc61417a7e7fe12
sml2198 <sarah.michael.levine@gmail.com>
2016-08-11 10:55:49 -0700

    fixes file name and another bug with mine_types merge

--- collapse_accidents(renameme6).R
+++ collapse_accidents(renameme6).R
@@ -9,13 +9,17 @@
 
 #####################################################################################################
 
+library(plyr)
+library(stringr)
+library(zoo)
+
 # define file names
 # input: coded accidents data (contains MR indicator - no PS indicator yet) produced in 5_analyze_MR.R
 mines_accidents_coded.in.file.name = "X:/Projects/Mining/NIOSH/analysis/data/4_coded/accidents_with_predictions.csv"
-# output: collapsed coded accidents data 
+  # output: collapsed coded accidents data 
 mines_accidents_coded.out.file.name = "X:/Projects/Mining/NIOSH/analysis/data/4_collapsed/collapsed_accidents.rds"
-# input: cleaned mine-types key produced in produced in 1_clean_mines.R
-mine.types.file.name = "X:/Projects/Mining/NIOSH/analysis/data/3_merged/merged_mines_accidents.rds"
+  # input: cleaned mine-types key produced in produced in 1_clean_mines.R
+mine.types.file.name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/mine_types.rds"
 
 ######################################################################################################################################
 
@@ -24,6 +28,11 @@ mine.types.file.name = "X:/Projects/Mining/NIOSH/analysis/data/3_merged/merged_m
 # Read in data
 mine_types = readRDS(mine.types.file.name)
 mines_accidents_coded = read.csv(mines_accidents_coded.in.file.name)
+
+# Remove unecessary vars
+mines_accidents_coded = mines_accidents_coded[, c(match("mineid", names(mines_accidents_coded)),
+                                                  match("accidentdate", names(mines_accidents_coded)),
+                                                  match("MR", names(mines_accidents_coded)))]
 
 # Format mineid by padding it with zeroes to make it 7 digits, so we have a clean merge
 mines_accidents_coded$mineid = str_pad(mines_accidents_coded$mineid, 7, pad = "0")
@@ -35,10 +44,14 @@ mines_accidents_coded$quarter = as.yearqtr(mines_accidents_coded$accidentdate)
 
 ######################################################################################################################################
 
-# READ MINE-TYPES DATA, MERGE ONTO ACCIDENTS, DROP OBSERVATIONS NOT RELEVANT TO THE STUDY ENVIRONMENT
+# COLLAPSE TO THE MINE-QUARTER LEVEL, THEN OUTPUT
 
-# Drop observations before our study period
-mines_accidents_coded = mines_accidents_coded[(mines_accidents_coded$quarter > "1999 Q4"),]
+# Create injury indicator so that we can collapse & sum total injuries per mine quarter
+mines_accidents_coded$totalinjuries = 1
+
+######################################################################################################################################
+
+# READ MINE-TYPES DATA, MERGE ONTO ACCIDENTS, DROP OBSERVATIONS NOT RELEVANT TO THE STUDY ENVIRONMENT
 
 # Merge on minetypes to drop non-coal and non-underground observations before saving
 mines_accidents_coded = merge(mines_accidents_coded, mine_types, by = c("mineid"), all = T)
@@ -49,15 +62,13 @@ rm(mine_types)
 
 # Only keep observations from environment we care about
 mines_accidents_coded = mines_accidents_coded[mines_accidents_coded$minetype == "Underground",]
-mines_accidents_coded = mines_accidents_coded[mines_accidents_coded$subunit == "UNDERGROUND",]
-mines_accidents_coded = mines_accidents_coded[mines_accidents_coded$coalcormetalmmine.x == "C",]
+# mines_accidents_coded = mines_accidents_coded[mines_accidents_coded$subunit == "UNDERGROUND",]
+mines_accidents_coded = mines_accidents_coded[mines_accidents_coded$coalcormetalmmine == "C",]
+
+# Drop observations before our study period
+mines_accidents_coded = mines_accidents_coded[(mines_accidents_coded$quarter > "1999 Q4"),]
 
 ######################################################################################################################################
-
-# COLLAPSE TO THE MINE-QUARTER LEVEL, THEN OUTPUT
-
-# Create injury indicator so that we can collapse & sum total injuries per mine quarter
-mines_accidents_coded$totalinjuries = 1
 
 # Collapse mines_accidents data here.
 summed_coded_accidents = ddply(mines_accidents_coded[, c(grep("totalinjuries", names(mines_accidents_coded)), 
@@ -66,8 +77,11 @@ summed_coded_accidents = ddply(mines_accidents_coded[, c(grep("totalinjuries", n
                                                          match("quarter", names(mines_accidents_coded)))], c("mineid", "quarter"), 
                                function(x) colSums(x[, c(grep("totalinjuries", names(x)), grep("MR", names(x)))], na.rm = T))
 
+######################################################################################################################################
+
 # Save and clear workspace
 saveRDS(summed_coded_accidents, mines_accidents_coded.out.file.name)
+
 rm(list = ls())
 gc()
 
