737f134d0150ae496448e79c167800792d32f5f7
juliabodson <juliabodson@gmail.com>
2016-08-16 11:38:09 -0700

    no inspections - breakdown over mine and quarters

--- 14_data_spelunking.R
+++ 14_data_spelunking.R
@@ -52,16 +52,22 @@ no_insp = data[data$num_insp == 0, c("mineid", "quarter")]
 # how many quarters with no inspections per mine?
 barplot(sort(table(no_insp$mineid)),
         xaxt = "n",
-        main = "Number of No-Inspection Quarters per Mine", 
+        main = "Number of No-Inspection Quarters by Mine", 
         xlab = "Mine ID",
         ylab = "Number of No-Inspection Quarters")
 
-# mines missing > + >= 10 quarters of data
-unique(no_insp[no_insp$mineid %in% names(which(table(no_insp$mineid) > 10)), "mineid"])
-unique(no_insp[no_insp$mineid %in% names(which(table(no_insp$mineid) == 10)), "mineid"])
+# mines with no inspections for > + >= 10 quarters of data
+mineid_10plus_missing = unique(no_insp[no_insp$mineid %in% names(which(table(no_insp$mineid) > 10)), "mineid"])
+mineid_10_missing = unique(no_insp[no_insp$mineid %in% names(which(table(no_insp$mineid) == 10)), "mineid"])
 
-# how many non-missing quarters do these mines contribute to data?
-data[data$mineid == "1202423", c("num_insp", "quarter")] # just an ex -- could automate and get info for all of these mines ^^ 
+# how many (non-missing) quarters do these mines contribute to data?
+dim(data[(data$mineid %in% mineid_10plus_missing & data$num_insp > 0), ]) # 497 obs
+dim(data[(data$mineid %in% mineid_10_missing & data$num_insp > 0), ]) # 1043 obs
 
 # which quarters are missing inspections
-hist(no_insp$quarter)
+barplot(table(no_insp$quarter), 
+        main = "Number of Mines with No Inspections by Quarter", 
+        xlab = "Quarter", 
+        ylab = "Number of Mines with No Inspections")
+
+
