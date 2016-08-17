1fc0d37cea0ee8a648aaa53c54aab6663b31762f
sml2198 <sarah.michael.levine@gmail.com>
2016-08-17 12:08:19 -0700

    BURN IT ALLLLLLL (all the inspecshours data)

--- 8_clean_inspections.R
+++ 8_clean_inspections.R
@@ -17,8 +17,6 @@ library(stringr)
 # define file names
   # input: inspections data
 open_data_inspecs_in_file_name = "X:/Projects/Mining/NIOSH/analysis/data/0_originals/MSHA/open_data/Inspections.txt"
-  # input: inspection hours data
-early_inspecs_hours_file_name = "X:/Projects/Mining/NIOSH/analysis/data/1_converted/MSHA/inspection_hours_fromText.csv"
   # input: clean mine type data (1_clean_mines)
 mine_types_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/mine_types.rds"
   # output: clean and merged inspections data, uniquely identified by mineid *eventno
@@ -111,54 +109,6 @@ clean_inspecs = clean_inspecs[!is.na(clean_inspecs$eventno), ] # back to 327775 
 # drop observations from environments not of interest
   # facility means a mill/processing location, always above ground, according to April Ramirez @ DOL on 6/6/16
 clean_inspecs = clean_inspecs[clean_inspecs$minetype == "Underground", ] # now we have 195,719 obs
-
-######################################################################################################
-
-# HONESTLY I HAVE NO IDEA WHY WE EVEN BROUGHT IN THIS INSPECTION HOURS DATA. WE HAVE NO MISSING INSPECTION
-# HOURS VARS IN THIS DATA SET FROM THE OPEN DATA PORTAL. I'M GOING TO TAKE OUT ALL OF THIS NONSENSE NOW.
-
-# READ AND CLEAN INSPECTION HOURS DATA 
-
-# read inspection hours data - 4634874 obs, 10 vars
-# early_inspecs_hours = read.csv(early_inspecs_hours_file_name)
-# 
-# # drop observations from envrionments not of interest
-# early_inspecs_hours = early_inspecs_hours[early_inspecs_hours$coalcormetalmmine == "C", ] # drop 1744657 obs
-# 
-# # rename variables
-# names(early_inspecs_hours)[names(early_inspecs_hours) == "sumtotalminingareatime"] = "sumtotal_on_site_hours"
-# names(early_inspecs_hours)[names(early_inspecs_hours) == "sumcalctotalinspectionrelatedtim"] = "sumtotal_insp_hours"
-# 
-# # format variables
-# early_inspecs_hours$eventno = as.character(early_inspecs_hours$eventno)
-# early_inspecs_hours[, "sumtotal_insp_hours"] = as.numeric(gsub(",", "", as.character(early_inspecs_hours$sumtotal_insp_hours), fixed = T))
-# early_inspecs_hours = aggregate(cbind(sumtotal_on_site_hours, sumtotal_insp_hours) ~ eventno + mineid + coalcormetalmmine, 
-#                                 FUN = "sum", data = early_inspecs_hours)
-# early_inspecs_hours$mineid = str_pad(early_inspecs_hours$mineid, 7, pad = "0")
-# early_inspecs_hours$eventno = str_pad(early_inspecs_hours$eventno, 7, pad = "0")
-
-# MERGE INSPECTION HOURS DATA WITH INSPECTIONS DATA, THEN OUTPUT
-
-# # create variables to check the merge results
-# clean_inspecs$mergecheck.inspec = 1
-# early_inspecs_hours$mergecheck.hrs = 1
-# 
-# # merge inspections data with open source inspection hours data - 1087895 obs, 52 vars
-# clean_inspecs = merge(clean_inspecs, early_inspecs_hours, by = c("mineid", "eventno"), all = T)
-# 
-# # check for conflicts in the merge
-# insp_conflcts = sum(!is.na(clean_inspecs[, "sumtotal_insp_hours.x"]) & (clean_inspecs[, "sumtotal_insp_hours.x"] != clean_inspecs[, "sumtotal_insp_hours.y"])) # 6 observations
-# on_site_conflcts = sum(!is.na(clean_inspecs[, "sumtotal_on_site_hours.x"]) & (clean_inspecs[, "sumtotal_on_site_hours.x"] != clean_inspecs[, "sumtotal_on_site_hours.y"])) # 2 observations
-# nonNA_conflicts = max(insp_conflcts, on_site_conflcts)
-# 
-# clean_inspecs = clean_inspecs[complete.cases(clean_inspecs$calendaryear),] # 194574 obs, 1934 mines, 52 vars
-# #clean_inspecs = clean_inspecs[(!is.na(clean_inspecs$mergecheck.hrs) & !is.na(clean_inspecs$mergecheck.inspec)), ] # drop 916126 obs
-# 
-# # rename duplicate variables
-# clean_inspecs = clean_inspecs[, -grep(".y", names(clean_inspecs), fixed = T)]
-# names(clean_inspecs)[c(grep(".x", names(clean_inspecs), fixed = TRUE))] = sub(".x", "", names(clean_inspecs)[c(grep(".x", names(clean_inspecs), fixed = TRUE))])
-# clean_inspecs = clean_inspecs[!(((!is.na(clean_inspecs$controllerid) & clean_inspecs$controllerid == "C11088") | is.na(clean_inspecs$controllerid)) & clean_inspecs$eventno == "4165469"),
-#                               c(-grep("coal_metal_ind", names(clean_inspecs)), -grep("merge", names(clean_inspecs)))]
 
 ######################################################################################################
 
