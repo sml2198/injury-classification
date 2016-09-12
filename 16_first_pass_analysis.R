e9bce40eb2ec353c9f36d68cdd384a9ca1ab7acc
sml2198 <sarah.michael.levine@gmail.com>
2016-09-12 14:48:49 -0700

    adds project

---
+++ 16_first_pass_analysis.R
@@ -0,0 +1,154 @@
+# NIOSH Project 2014-N-15776
+
+# 16 - Who am I?
+  # What do I do?
+
+# Last edit 9/12/16
+
+"X:/Projects/Mining/NIOSH/analysis/data/5_prediction-ready/MR_prediction_data.rds"
+
+######################################################################################################
+
+# Online Resources
+
+# General GLM
+  # http://www.utstat.toronto.edu/~brunner/oldclass/2201s11/readings/glmbook.pdf
+  # http://www.statmethods.net/advstats/glm.html
+  # http://www.uphs.upenn.edu/dgimhsr/documents/ispor15.glmworkshop.glick.2.pdf
+
+# Poisson GLM
+  # https://onlinecourses.science.psu.edu/stat504/node/168
+  # https://onlinecourses.science.psu.edu/stat504/node/169
+  # http://datavoreconsulting.com/programming-tips/count-data-glms-choosing-poisson-negative-binomial-zero-inflated-poisson/
+  # http://www.biostat.umn.edu/~dipankar/bmtry711.11/lecture_13.pdf
+  # http://stats.stackexchange.com/questions/176918/poisson-vs-quasi-poisson
+  # http://stats.stackexchange.com/questions/66586/is-there-a-test-to-determine-whether-glm-overdispersion-is-significant
+  # http://data.princeton.edu/wws509/notes/c4.pdf
+
+# Probit/Logit GLM
+  # http://www.biostat.umn.edu/~dipankar/bmtry711.11/lecture_12.pdf
+  # http://stats.stackexchange.com/questions/20523/difference-between-logit-and-probit-models/30909#30909
+
+# Other Modeling Options
+  # https://core.ac.uk/download/pdf/6303254.pdf
+
+######################################################################################################
+
+# Model Label Key: X.Y.Z.N
+  # X
+    # P: predictors are part-level violations
+    # SP: predictors are subpart-level violations
+  # Y
+    # C: response variable is count of injuries
+    # B: response variable is binary of injuries
+  # Z
+    # V: predictors are number of violations
+    # SSV: predictors are number of significant and substantial violations
+    # PP: predictors are number of penalty points for violations
+  # N
+    # 1: inj_t ~ viol_t
+    # 2: inj_t ~ viol_(t_-1)
+    # 3: inj_t ~ viol_(annual avg pre_t)
+    # 4: inj_t ~ viol_(avg since study period)
+
+######################################################################################################
+
+# Running and Testing Models
+
+# Model P.C.V.1
+
+# Model P.C.V.2
+
+# Model P.C.V.3
+
+# Model P.C.V.4
+
+# Model P.C.SSV.1
+
+# Model P.C.SSV.2
+
+# Model P.C.SSV.3
+
+# Model P.C.SSV.4
+
+# Model P.C.PP.1
+
+# Model P.C.PP.2
+
+# Model P.C.PP.3
+
+# Model P.C.PP.4
+
+# Model P.B.V.1
+
+# Model P.B.V.2
+
+# Model P.B.V.3
+
+# Model P.B.V.4
+
+# Model P.B.SSV.1
+
+# Model P.B.SSV.2
+
+# Model P.B.SSV.3
+
+# Model P.B.SSV.4
+
+# Model P.B.PP.1
+
+# Model P.B.PP.2
+
+# Model P.B.PP.3
+
+# Model P.B.PP.4
+
+# Model SP.C.V.1
+
+# Model SP.C.V.2
+
+# Model SP.C.V.3
+
+# Model SP.C.V.4
+
+# Model SP.C.SSV.1
+
+# Model SP.C.SSV.2
+
+# Model SP.C.SSV.3
+
+# Model SP.C.SSV.4
+
+# Model SP.C.PP.1
+
+# Model SP.C.PP.2
+
+# Model SP.C.PP.3
+
+# Model SP.C.PP.4
+
+# Model SP.B.V.1
+
+# Model SP.B.V.2
+
+# Model SP.B.V.3
+
+# Model SP.B.V.4
+
+# Model SP.B.SSV.1
+
+# Model SP.B.SSV.2
+
+# Model SP.B.SSV.3
+
+# Model SP.B.SSV.4
+
+# Model SP.B.PP.1
+
+# Model SP.B.PP.2
+
+# Model SP.B.PP.3
+
+# Model SP.B.PP.4
+
+######################################################################################################
