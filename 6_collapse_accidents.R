90180f2608358686b968be9c1865df92f399f45c
Sarah Levine <sml2198@users.noreply.github.com>
2016-09-13 09:10:56 -0700

    Update and rename collapse_accidents(renameme6).R to 6_collapse_accidents.R

--- 6_collapse_accidents.R
+++ 6_collapse_accidents.R
@@ -1,6 +1,6 @@
 # NIOSH Project 2014-N-15776
 
-# 10 - Collapse Accidents 
+# 6 - Collapse Accidents Data
 # Loads in coded accidents data from 5_analyze_MR_R.R and formats variables for merge
 # Loads and merges minetype data and drops observations not relevant to the study environment
 # Collapses to the mine-quarter level, the outputs
