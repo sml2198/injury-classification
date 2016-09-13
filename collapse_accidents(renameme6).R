34c40081a80cad2fae4ab652cc72e44da6a99e54
sml2198 <sarah.michael.levine@gmail.com>
2016-09-13 09:07:17 -0700

    makes this file MR/PS convertible

--- collapse_accidents(renameme6).R
+++ collapse_accidents(renameme6).R
@@ -14,12 +14,24 @@ library(stringr)
 library(zoo)
 
 # define file names
-# input: coded accidents data (contains MR indicator - no PS indicator yet) produced in 5_analyze_MR.R
-mines_accidents_coded_in_file_name = "X:/Projects/Mining/NIOSH/analysis/data/4_coded/accidents_with_predictions.csv"
-  # output: collapsed coded accidents data 
-mines_accidents_coded_out_file_name = "X:/Projects/Mining/NIOSH/analysis/data/4_collapsed/collapsed_MR_accidents.rds"
   # input: cleaned mine-types key produced in produced in 1_clean_mines.R
 mine_types_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/mine_types.rds"
+  # input: coded MR accidents data produced in 5_analyze_MR.R
+MR_accidents_coded_in_file_name = "X:/Projects/Mining/NIOSH/analysis/data/4_coded/MR_accidents_with_predictions.csv"
+  # input: coded PS accidents data produced in 4_analyze_PS.R
+PS_accidents_coded_in_file_name = "X:/Projects/Mining/NIOSH/analysis/data/4_coded/PS_accidents_with_predictions.csv"
+
+  # output: collapsed coded accidents data 
+MR_accidents_coded_out_file_name = "X:/Projects/Mining/NIOSH/analysis/data/4_collapsed/collapsed_MR_accidents.rds"
+# output: collapsed coded accidents data 
+PS_accidents_coded_out_file_name = "X:/Projects/Mining/NIOSH/analysis/data/4_collapsed/collapsed_PS_accidents.rds"
+
+######################################################################################################################################
+
+# Set preferences 
+
+# injury.type = "PS"
+ injury.type = "MR"
 
 ######################################################################################################################################
 
@@ -27,12 +39,23 @@ mine_types_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/mine_ty
 
 # Read in data
 mine_types = readRDS(mine_types_file_name)
-mines_accidents_coded = read.csv(mines_accidents_coded_in_file_name)
 
-# Remove unecessary vars
-mines_accidents_coded = mines_accidents_coded[, c(match("mineid", names(mines_accidents_coded)),
-                                                  match("accidentdate", names(mines_accidents_coded)),
-                                                  match("MR", names(mines_accidents_coded)))]
+if (injury.type == "PS"){
+  mines_accidents_coded = read.csv(PS_accidents_coded_in_file_name)
+  
+  # Remove unecessary vars
+  mines_accidents_coded = mines_accidents_coded[, c(match("mineid", names(mines_accidents_coded)),
+                                                    match("accidentdate", names(mines_accidents_coded)),
+                                                    match("PS", names(mines_accidents_coded)))]
+}
+if (injury.type == "MR"){
+  mines_accidents_coded = read.csv(MR_accidents_coded_in_file_name)
+  
+  # Remove unecessary vars
+  mines_accidents_coded = mines_accidents_coded[, c(match("mineid", names(mines_accidents_coded)),
+                                                    match("accidentdate", names(mines_accidents_coded)),
+                                                    match("MR", names(mines_accidents_coded)))]
+}
 
 # Format mineid by padding it with zeroes to make it 7 digits, so we have a clean merge
 mines_accidents_coded$mineid = str_pad(mines_accidents_coded$mineid, 7, pad = "0")
@@ -51,13 +74,18 @@ mines_accidents_coded = mines_accidents_coded[mines_accidents_coded$quarter > "1
                                                 mines_accidents_coded$quarter < "2016 Q2",]
 
 # Create injury indicator so that we can collapse & sum total injuries per mine quarter
-mines_accidents_coded$totalinjuries = 1
+mines_accidents_coded$total_injuries = 1
 
 # Merge on minetypes to drop non-coal and non-underground observations before saving
 mines_accidents_coded = merge(mines_accidents_coded, mine_types, by = c("mineid"), all = T)
 
 # Drop non-merging observations
-mines_accidents_coded = mines_accidents_coded[!is.na(mines_accidents_coded$MR),]
+if (injury.type == "PS"){
+  mines_accidents_coded = mines_accidents_coded[!is.na(mines_accidents_coded$PS),]
+}
+if (injury.type == "MR"){
+  mines_accidents_coded = mines_accidents_coded[!is.na(mines_accidents_coded$MR),]
+}
 rm(mine_types)
 
 # Only keep observations from environment we care about
@@ -67,16 +95,30 @@ mines_accidents_coded = mines_accidents_coded[mines_accidents_coded$coalcormetal
 ######################################################################################################################################
 
 # Collapse mines_accidents data here.
-summed_coded_accidents = ddply(mines_accidents_coded[, c(grep("totalinjuries", names(mines_accidents_coded)), 
-                                                         grep("MR", names(mines_accidents_coded)),
-                                                         match("mineid", names(mines_accidents_coded)), 
-                                                         match("quarter", names(mines_accidents_coded)))], c("mineid", "quarter"), 
-                               function(x) colSums(x[, c(grep("totalinjuries", names(x)), grep("MR", names(x)))], na.rm = T))
+if (injury.type == "PS"){
+  summed_coded_accidents = ddply(mines_accidents_coded[, c(grep("total_injuries", names(mines_accidents_coded)), 
+                                                           grep("PS", names(mines_accidents_coded)),
+                                                           match("mineid", names(mines_accidents_coded)), 
+                                                           match("quarter", names(mines_accidents_coded)))], c("mineid", "quarter"), 
+                                 function(x) colSums(x[, c(grep("total_injuries", names(x)), grep("PS", names(x)))], na.rm = T))
+}
+if (injury.type == "MR"){
+  summed_coded_accidents = ddply(mines_accidents_coded[, c(grep("total_injuries", names(mines_accidents_coded)), 
+                                                           grep("MR", names(mines_accidents_coded)),
+                                                           match("mineid", names(mines_accidents_coded)), 
+                                                           match("quarter", names(mines_accidents_coded)))], c("mineid", "quarter"), 
+                                 function(x) colSums(x[, c(grep("total_injuries", names(x)), grep("MR", names(x)))], na.rm = T))
+}
 
 ######################################################################################################################################
 
 # Save and clear workspace
-saveRDS(summed_coded_accidents, mines_accidents_coded_out_file_name)
+if (injury.type == "PS"){
+  saveRDS(summed_coded_accidents, PS_accidents_coded_out_file_name)
+}
+if (injury.type == "MR"){
+  saveRDS(summed_coded_accidents, MR_accidents_coded_out_file_name)
+}
 
 rm(list = ls())
 gc()
