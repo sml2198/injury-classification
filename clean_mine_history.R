fc77036e8779ad1fcdd3e9e2df4e71a13c768400
sml2198 <sarah.michael.levine@gmail.com>
2016-09-13 14:22:27 -0700

    still figuring out what to do here

--- clean_mine_history.R
+++ clean_mine_history.R
@@ -17,6 +17,34 @@ history_out_file_name = "X:/Projects/Mining/NIOSH/analysis/data/2_cleaned/clean_
 
 ######################################################################################################
 
+# load data - 144065 obs, 13 vars
 history = read.table(history_in_file_name, header = T, sep = "|", na.strings=c("","NA"))
+
+# rename vars to make them prettier
+names(history) = tolower(names(history))
+names(history)[names(history) == "mine_id"] = "mineid"
+names(history)[names(history) == "mine_name"] = "minename"
+names(history)[names(history) == "mine_status"] = "minestatus"
+names(history)[names(history) == "controller_id"] = "controllerid"
+names(history)[names(history) == "operator_id"] = "operatorid"
+
+# format variables
+history$controllerid = as.character(history$controllerid)
+history$operatorid = as.character(history$operatorid)
+history$mineid = as.character(history$mineid)
+history$controllerid = str_pad(history$controllerid, 7, pad = "0")
+history$operatorid = str_pad(history$operatorid, 7, pad = "0")
+history$mineid = str_pad(history$mineid, 7, pad = "0")
+
+# drop obvs from wrong environment - now 63143 obs
+history = history[history$coal_metal_ind == "C", ] 
+
+# format date vars
+history$controller_start_dt = as.Date(as.character(history$operator_start_dt), "%m/%d/%Y")
+history$controller_end_dt = as.Date(as.character(history$operator_start_dt), "%m/%d/%Y")
+history$operator_start_dt = as.Date(as.character(history$operator_start_dt), "%m/%d/%Y")
+history$operator_end_dt = as.Date(as.character(history$operator_start_dt), "%m/%d/%Y")
+
+history = history[, c(-grep("_dt", names(history)))]
 
 ######################################################################################################
